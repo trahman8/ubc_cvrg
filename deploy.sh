19fe30a9e803565f4f0a296085a8577c4854df1d
Gursimran <simar.i3r@gmail.com>
2019-10-06 10:59:04 -0700

    Updated the deploy script to better update the progress and removed a messy bug about file names

--- deploy.sh
+++ deploy.sh
@@ -2,19 +2,24 @@
 umask 002
 
 # Fetch the latest sources
+echo "## Reseting code to origin ##"
 git pull origin
+git pull origin gh-pages
 
 # Use the right config file (/ubc_cvrg removed)
+echo "## Cleaning up existing files ##"
 mv _config.yml _config_backup.yml
 mv _config_deploy.yml _config.yml
+rm -rf /home/msimar/ubc_cvrg/_site
 
 # Build the site
 ## bundle install
 ## bundle exec jekyll serve
-rm -rf /home/msimar/ubc_cvrg/_site
+echo "## Building Website using jekyl ##"
 jekyll build
 
 # Take backup and clean the folder
+echo "## Backup (~/webpage_backup) and clean the destination host directory ##"
 rm -rf ~/webpage_backup
 mkdir ~/webpage_backup
 cp -rf /var/www/html/vision ~/webpage_backup
@@ -27,4 +32,7 @@ rm -rf /var/www/html/vision/*
 # find /var/www/html/vision -type f -exec chmod 664 {} \;
 
 # copy all files
+echo "## Host the newly built website at /var/www/html/vision/ ##" 
 cp -r /home/msimar/ubc_cvrg/_site/* /var/www/html/vision/
+echo "######## DONE ########"
+echo "\nNOTE: If you added new files (pics/ scripts), you may have to change permission. Permission should be 755 for directories and 664 for files"
